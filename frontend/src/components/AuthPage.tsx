17add2079cd326a2f1855055d278de285c6ce6a9
Ashish Bhungagni <bhunganiashish@gmail.com>
2025-12-11 10:13:00 +0530

    working version

--- frontend/src/components/AuthPage.tsx
+++ frontend/src/components/AuthPage.tsx
@@ -15,8 +15,7 @@ import { saveUserSession } from '@/lib/persistentAuth';
 
 
 export default function AuthPage() {
-  let { address } = useAppKitAccount();
-  address = "0xAd5292D3D35F57CC0D7876cfD7B583DC99637b0d"
+  const { address } = useAppKitAccount();
   const { signMessageAsync } = useSignMessage();
   const { setCurrentUser, setAuthenticated } = useAppStore();
   const [isConnecting, setIsConnecting] = useState(false);
